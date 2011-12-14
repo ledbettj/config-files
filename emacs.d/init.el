ac8a84e3b4f33fc0d6f46a681b4ba21f5a4c7469
John Ledbetter <jledbetter@vitrue.com>
2011-12-13 21:29:30 -0500

    fix typo in linux font setting

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -240,7 +240,7 @@
                                                   "magenta4" "cyan3" "white"])
 (if (eq system-type 'darwin)
     (setq default-frame-alist '((font . "Droid Sans Mono-14")))
-  (setq default-frame-alist '(("Droid Sans Mono-12"))))
+  (setq default-frame-alist '((font . "Droid Sans Mono-12"))))
 
 (setq frame-title-format   ;; frame title: user@host: buffer [modified?]
   (list
