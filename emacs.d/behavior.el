d98e2739308195c9e8e7aa5ab8213da0bf33c1c0
John Ledbetter <jledbetter@vitrue.com>
2012-06-21 12:48:52 -0400

    change autosave directory to not be annoying.

--- emacs.d/behavior.el
+++ emacs.d/behavior.el
@@ -7,6 +7,12 @@
 
 (defalias 'yes-or-no-p 'y-or-n-p) ; always ask Y/N? instead of yes/no.
 
+; stop writing annoying autosave files to the current directory.
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
 (defun toggle-fullscreen ()
   "Switch between fullscreen and windowed mode"
   (interactive)
