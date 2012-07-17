d0151fc32f17df525cd2325723350c0478deed0a
John Ledbetter <jledbetter@vitrue.com>
2012-07-17 11:08:53 -0400

    fix m-ret to M-return

--- emacs.d/customization/keybinds.el
+++ emacs.d/customization/keybinds.el
@@ -1,4 +1,4 @@
-(global-set-key [M-g]     'goto-line)
-(global-set-key [M-up]    'move-text-up)
-(global-set-key [M-down]  'move-text-down)
-(global-set-key [m-ret]   'toggle-fullscreen)
+(global-set-key [M-g]      'goto-line)
+(global-set-key [M-up]     'move-text-up)
+(global-set-key [M-down]   'move-text-down)
+(global-set-key [M-return] 'toggle-fullscreen)
