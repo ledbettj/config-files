10f12f23bac0c024c886d95bcbfa3574811d714a
John Ledbetter <john.ledbetter@oracle.com>
2012-10-09 13:34:14 -0400

    left/right toggle between matches in iswitchb mode.

--- emacs.d/custom/keybinds.el
+++ emacs.d/custom/keybinds.el
@@ -8,3 +8,12 @@
 (global-set-key [C-kp-delete] 'kill-word)
 (global-set-key [backspace]   'backward-delete-char-hungry)
 
+; make right and left arrow pick matching buffers in iswitchb-mode
+(defun iswitchb-local-keys ()
+  (mapc (lambda (K)
+          (let* ((key (car K)) (fun (cdr K)))
+            (define-key iswitchb-mode-map (edmacro-parse-keys key) fun)))
+    '(("<right>" . iswitchb-next-match)
+       ("<left>"  . iswitchb-prev-match))))
+
+(add-hook 'iswitchb-define-mode-map-hook 'iswitchb-local-keys)
