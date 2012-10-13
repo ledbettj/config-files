e139163dd096d926395deb0a0ff2e16b84534a84
John Ledbetter <john.ledbetter@gmail.com>
2012-10-13 13:01:23 -0400

    add reopen-file-with-sudo which does what it says.

--- emacs.d/custom/behavior.el
+++ emacs.d/custom/behavior.el
@@ -57,3 +57,13 @@
   (interactive "*p\nP")
   (let ((backward-delete-char-untabify-method 'hungry))
     (backward-delete-char-untabify arg killp)))
+
+
+(defun reopen-file-with-sudo ()
+  "Open the currently visited file as root via sudo."
+  (interactive)
+  (if (buffer-file-name)
+    (let ((file-name (buffer-file-name)))
+      (kill-buffer (current-buffer))
+      (find-file (concat "/sudo:root@localhost:" file-name))
+      (message "now editing %s as root" file-name))))
