838b9919059bb36cdc9f9af354f1f090322f6400
John Ledbetter <john@throttle.io>
2014-04-14 15:01:05 -0400

    dont use monokai without a window

--- emacs.d/custom/ui.el
+++ emacs.d/custom/ui.el
@@ -123,4 +123,7 @@ than the background of the buffer."
      (set-face-foreground 'diff-added "green3")
      (set-face-foreground 'diff-removed "red4")))
 
-(load-theme 'monokai t nil)
+;; monokai has a green background in a terminal. don't use it.
+(if (window-system)
+  (load-theme 'monokai t nil)
+  (load-theme 'wombat  t nil))
