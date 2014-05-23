82d889f03e85cd12437599f081b272fd77a43edd
John Ledbetter <john@callrail.com>
2014-05-23 11:26:10 -0400

    tweak window layout

--- slate.js
+++ slate.js
@@ -3,7 +3,7 @@ var left = '0', right = '1';
 slate.config('defaultToCurrentScreen', false);
 
 var leftFull = function(monitor) {
-  return slate.operation('move',  {
+  return slate.operation('move', {
     screen: monitor,
     x: 'screenOriginX',
     y: 'screenOriginY',
@@ -13,7 +13,7 @@ var leftFull = function(monitor) {
 };
 
 var rightFull = function(monitor) {
-  return slate.operation('move',  {
+  return slate.operation('move', {
     screen: monitor,
     x: 'screenOriginX+screenSizeX/2',
     y: 'screenOriginY',
@@ -22,10 +22,40 @@ var rightFull = function(monitor) {
   });
 };
 
+var topFull = function(monitor) {
+  return slate.operation('move', {
+    screen: monitor,
+    x: 'screenOriginX',
+    y: 'screenOriginY',
+    width: 'screenSizeX',
+    height: 'screenSizeY/2'
+  });
+};
+
+var bottomFull = function(monitor) {
+  return slate.operation('move', {
+    screen: monitor,
+    x: 'screenOriginX',
+    y: 'screenOriginY+screenSizeY/2',
+    width: 'screenSizeX',
+    height: 'screenSizeY/2'
+  });
+};
+
 var topRight = function(monitor) {
-  return slate.operation('move',  {
+  return slate.operation('move', {
     screen: monitor,
     x: 'screenOriginX+screenSizeX/2',
+    y: 'screenOriginY',
+    width: 'screenSizeX/2',
+    height: 'screenSizeY/2'
+  });
+};
+
+var topLeft = function(monitor) {
+  return slate.operation('move', {
+    screen: monitor,
+    x: 'screenOriginX',
     y: 'screenOriginY',
     width: 'screenSizeX/2',
     height: 'screenSizeY/2'
@@ -42,13 +72,23 @@ var bottomRight = function(monitor) {
   });
 };
 
+var bottomLeft = function(monitor) {
+  return slate.operation('move',  {
+    screen: monitor,
+    x: 'screenOriginX',
+    y: 'screenOriginY+screenSizeY/2',
+    width: 'screenSizeX/2',
+    height: 'screenSizeY/2'
+  });
+};
+
 slate.layout('worksies', {
   iTerm:           { operations: [bottomRight(right)] },
   'Google Chrome': { operations: [topRight(right)]    },
   Emacs:           { operations: [leftFull(right)]    },
-  Spotify:         { operations: [leftFull(left)]     },
-  HipChat:         { operations: [topRight(left)]     },
-  Mail:            { operations: [bottomRight(left)]  }
+  Spotify:         { operations: [bottomFull(left)]   },
+  HipChat:         { operations: [topLeft(left)]      },
+  Mail:            { operations: [topRight(left)]     }
 });
 
 slate.bind('1:cmd', slate.operation('layout', {name: 'worksies'}));
