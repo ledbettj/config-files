720cbff74581360f3a688fbb2379020b74163d31
John Ledbetter <john@callrail.com>
2014-06-11 09:19:30 -0400

    rainbow-mode in js-mode

--- emacs.d/custom/js.el
+++ emacs.d/custom/js.el
@@ -1,7 +1,11 @@
 ;;; js.el - js-mode customizations for emacs
 ;; John Ledbetter <john.ledbetter@gmail.com>
 
-(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(add-hook 'js-mode-hook
+  (lambda ()
+    (rainbow-mode)
+    (tern-mode t)))
+
 (eval-after-load 'tern
    '(progn
       (require 'tern-auto-complete)
