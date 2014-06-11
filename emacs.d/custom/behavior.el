598a843be9751d34e6d716c79099864624bc6e08
John Ledbetter <john@callrail.com>
2014-06-11 09:17:13 -0400

    remove uneccessary root@localhost in reopen-file-with-sudo

--- emacs.d/custom/behavior.el
+++ emacs.d/custom/behavior.el
@@ -75,7 +75,7 @@
   (if (buffer-file-name)
     (let ((file-name (buffer-file-name)))
       (kill-buffer (current-buffer))
-      (find-file (concat "/sudo:root@localhost:" file-name))
+      (find-file (concat "/sudo::" file-name))
       (message "now editing %s as root" file-name))))
 
 (defun filter (condp lst)
