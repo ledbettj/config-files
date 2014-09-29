cd40af855aae748002a577991694136b4005cdbb
John Ledbetter <john@callrail.com>
2014-09-29 10:56:24 -0400

    byte compile elisp files after save.

--- emacs.d/custom/modes.el
+++ emacs.d/custom/modes.el
@@ -54,3 +54,12 @@
      (flyspell-mode t)))
 
 (add-hook 'dired-mode-hook 'rspec-dired-mode)
+
+(defun byte-compile-current-buffer ()
+  "`byte-compile' current buffer if it's emacs-lisp-mode and compiled file exists."
+  (interactive)
+  (when (and (eq major-mode 'emacs-lisp-mode)
+             (file-exists-p (byte-compile-dest-file buffer-file-name)))
+    (byte-compile-file buffer-file-name)))
+
+(add-hook 'after-save-hook 'byte-compile-current-buffer)
