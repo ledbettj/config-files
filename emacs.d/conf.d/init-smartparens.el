eea98bda1251752cb403cc86f184814e61ad2bf6
John Ledbetter <john@callrail.com>
2016-03-27 11:38:23 -0400

    load smart parens config to prevent single quote pairs in elisp mode

--- emacs.d/conf.d/init-smartparens.el
+++ emacs.d/conf.d/init-smartparens.el
@@ -5,4 +5,5 @@
   (add-hook 'prog-mode-hook #'smartparens-mode)
   :config
   (setq-default sp-autoescape-string-quote nil)
-  (setq-default sp-highlight-pair-overlay  nil))
+  (setq-default sp-highlight-pair-overlay  nil)
+  (require 'smartparens-config))
