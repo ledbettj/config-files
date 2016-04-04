b6e275752678ae056a584a8ed0ea1b9324e7494b
John Ledbetter <john@callrail.com>
2016-04-04 17:11:23 -0400

    fix paths when launched via launcher on OS X

---
+++ emacs.d/conf.d/init-paths.el
@@ -0,0 +1,35 @@
+;; when opening Emacs from the launcher in OS X, some useful paths are not set.
+;; this file remedies that.
+
+(defun env-path ()
+  "returns a list containing all the directorys in the user's PATH environmental
+variable."
+  (split-string (getenv "PATH") ":"))
+
+(defun path-env-string (paths)
+  "join the paths in the provided list into a string of the form x:y:z"
+  (mapconcat 'identity paths ":"))
+
+(defun path-contains-p (path)
+  "returns t if path is in the user's PATH."
+  (if (member path (env-path)) t nil))
+
+(defun path-prepend (path)
+  "append the given path to the PATH variable unless it's already present."
+  (unless (or (path-contains-p path) (not (file-exists-p path)))
+    (setenv "PATH"
+      (path-env-string
+        (append (list path) (env-path)))))
+  (add-to-list 'exec-path path))
+
+(defun path-append (path)
+  "prepend the given path to the PATH variable unless it's already present."
+  (unless (or (path-contains-p path) (not (file-exists-p path)))
+    (setenv "PATH"
+      (path-env-string
+        (append (env-path) (list path)))))
+  (add-to-list 'exec-path path t))
+
+(path-prepend (expand-file-name "~/.rbenv/bin"))
+(path-prepend (expand-file-name "~/.rbenv/shims"))
+(path-append  "/usr/local/bin")
