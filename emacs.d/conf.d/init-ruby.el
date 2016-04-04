d80d61a51840357245ebf4335a0042a205881617
John Ledbetter <john@callrail.com>
2016-04-04 14:24:34 -0400

    add back convert-hash-rocket

--- emacs.d/conf.d/init-ruby.el
+++ emacs.d/conf.d/init-ruby.el
@@ -1,6 +1,17 @@
 (require 'use-package)
 
+(defun convert-hash-rocket (BEG END)
+  "Convert hash rocket syntax to JSON syntax"
+  (interactive "r")
+  (if (not (region-active-p))
+    (message "mark not active")
+    (save-excursion
+      (goto-char BEG)
+      (while (re-search-forward ":\\([^\s]+\\)\s*=>\s*\\([^\s]+\\)" END t)
+        (replace-match "\\1: \\2")))))
+
 (use-package enh-ruby-mode :ensure t :pin melpa
+  :bind (("C-c r" . convert-hash-rocket))
   :diminish "Ruby+"
   :init
   (setq auto-mode-alist
