cbdedcf36c7eb4381af075c88b269fb4a8f63d6a
John Ledbetter <john@callrail.com>
2016-05-23 11:31:02 -0400

    don't start smartparens in web-mode

--- emacs.d/conf.d/init-smartparens.el
+++ emacs.d/conf.d/init-smartparens.el
@@ -1,7 +1,9 @@
 (use-package smartparens :ensure t :pin melpa
   :diminish " Ⓢ"
   :init
-  (add-hook 'prog-mode-hook #'smartparens-mode)
+  (add-hook 'prog-mode-hook '(lambda ()
+                               (unless (eq major-mode 'web-mode)
+                                 (smartparens-mode t))))
   :config
   (setq-default sp-autoescape-string-quote nil)
   (setq-default sp-highlight-pair-overlay  nil)
