61ef374dbf5c3903d1fc67c7ce58ee2739c09e12
John Ledbetter <john@callrail.com>
2016-05-24 09:04:15 -0400

    remove js3-mode now that bug#23603 is fixed

--- emacs.d/conf.d/init-js.el
+++ emacs.d/conf.d/init-js.el
@@ -2,15 +2,7 @@
   :config
   (setq-default js-indent-level 2))
 
-(use-package js3-mode :ensure t :pin melpa
-  :config
-  (setq-default js3-indent-level 2)
-  (setq-default js3-indent-dots t)
-  (setq-default js3-lazy-dots t)
-  )
-
 (use-package tern :ensure t :pin melpa
   :diminish " Ⓣ"
   :init
-  (add-hook 'js3-mode-hook #'tern-mode)
   (add-hook 'js-mode-hook #'tern-mode))
