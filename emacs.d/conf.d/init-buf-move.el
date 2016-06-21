ad7b89e829996a8f5fcb1e9cfc58c0d0c100b7e4
John Ledbetter <john@callrail.com>
2016-06-21 13:25:20 -0400

    remove buf-move

--- emacs.d/conf.d/init-buf-move.el
+++
@@ -1,5 +0,0 @@
-(use-package buffer-move :ensure t :pin melpa
-  :bind (([M-s-up]    . buf-move-up)
-         ([M-s-down]  . buf-move-down)
-         ([M-s-left]  . buf-move-left)
-         ([M-s-right] . buf-move-right)))
