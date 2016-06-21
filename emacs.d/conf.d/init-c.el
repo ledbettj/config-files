390dac2bf3e5970e260ada5fd45e2950b3e6c5e3
John Ledbetter <john@callrail.com>
2016-06-21 13:37:29 -0400

    get c-mode hungry deletin.

---
+++ emacs.d/conf.d/init-c.el
@@ -0,0 +1,4 @@
+(use-package cc-mode
+  :config
+  (setq-default c-default-style "linux")
+  (setq-default c-basic-offset 4))
--- emacs.d/conf.d/init-hungry-delete.el
+++ emacs.d/conf.d/init-hungry-delete.el
@@ -1,5 +1,11 @@
 (use-package hungry-delete :ensure t :pin melpa
   :diminish " ⓗ"
+  :init
+  (add-hook 'c-mode-hook
+            '(lambda ()
+               (turn-on-hungry-delete-mode)
+               (local-set-key (kbd "<backspace>") 'hungry-delete-backward)
+               ))
   :config
   (setq hungry-delete-chars-to-skip " \t\r\f\v")
   (global-hungry-delete-mode))
