e40ca9aa8e855253d00a1e392ae41e0811f51876
John Ledbetter <john@callrail.com>
2016-06-28 10:59:37 -0400

    update hammerspoon config

--- hammerspoon.lua
+++ hammerspoon.lua
@@ -1,12 +1,3 @@
-
-hs.application = require "hs.application"
-hs.window      = require "hs.window"
-hs.hotkey      = require "hs.hotkey"
-hs.fnutils     = require "hs.fnutils"
-hs.geometry    = require "hs.geometry"
-hs.screen      = require "hs.screen"
-hs.grid        = require "hs.grid"
-
 hs.window.animationDuration = 0 -- animations are dumb. move my windows fast.
 hs.grid.setGrid('4x4')
 
@@ -20,52 +11,19 @@ local BOT_LEFT   = "0,2,2,4"
 local BOT_FULL   = "0,2,4,4"
 local BOT_RIGHT  = "2,2,4,4"
 
--- find the main window belonging to the application with title 'title'
-function winFromTitle(title)
-   local apps = hs.application.runningApplications()
-   for _, app in pairs(apps) do
-      if app:title() == title then
-         return app:mainWindow()
-      end
-   end
-
-   return nil
-end
-
--- return a function that moves the front window to a grid position.
+-- return a function that moves the front window to a grid position,
+-- and focuses it if it's not focused.
 function moveToGrid(grid)
    return function()
-      hs.grid.set(hs.window.frontmostWindow(), grid)
+      local win = hs.window.frontmostWindow()
+      hs.grid.set(win, grid)
+      if not hs.window.focusedWindow() then
+         win:focus()
+      end
    end
 end
 
--- move the given window to the given screen, keeping the same relative
--- dimensions and placement.
-function winToScreen(win, screen)
-   if not win or not screen then
-      return false
-   end
-
-   local old_screen       = win:screen()
-   local old_screen_frame = old_screen:frame()
-   local win_frame        = win:frame()
-   local new_screen       = screen
-   local new_frame        = new_screen:frame()
-   local off_x_pct        = (win_frame.x - old_screen_frame.x) / (old_screen_frame.w)
-   local off_y_pct        = (win_frame.y - old_screen_frame.y) / (old_screen_frame.h)
-
-   local w_pct = win_frame.w / old_screen_frame.w
-   local h_pct = win_frame.h / old_screen_frame.h
-
-   win_frame.x = new_frame.x + (off_x_pct * new_frame.w)
-   win_frame.y = new_frame.y + (off_y_pct * new_frame.h)
-   win_frame.w = w_pct * new_frame.w
-   win_frame.h = h_pct * new_frame.h
-
-   win:setFrame(win_frame)
-end
-
-hs.hotkey.bind({"ctrl", "alt"}, "pad*",
+hs.hotkey.bind({"ctrl", "cmd"}, "pad*",
    function()
       local screens = hs.screen.allScreens()
       local apps = {
@@ -76,28 +34,58 @@ hs.hotkey.bind({"ctrl", "alt"}, "pad*",
       }
 
       for name, pos in pairs(apps) do
-         local w = winFromTitle(name)
-         if w then
-            hs.grid.set(w, pos[1], pos[2])
+         local app = hs.application.find(name)
+         local win = nil
+         if app then
+            hs.grid.set(app:mainWindow(), pos[1], pos[2])
          end
       end
    end
 )
 
-hs.hotkey.bind({"ctrl", "alt"}, "pad7", moveToGrid(TOP_LEFT))
-hs.hotkey.bind({"ctrl", "alt"}, "pad8", moveToGrid(TOP_FULL))
-hs.hotkey.bind({"ctrl", "alt"}, "pad9", moveToGrid(TOP_RIGHT))
-hs.hotkey.bind({"ctrl", "alt"}, "pad4", moveToGrid(FULL_LEFT))
-hs.hotkey.bind({"ctrl", "alt"}, "pad5", moveToGrid(FULL_FULL))
-hs.hotkey.bind({"ctrl", "alt"}, "pad6", moveToGrid(FULL_RIGHT))
-hs.hotkey.bind({"ctrl", "alt"}, "pad1", moveToGrid(BOT_LEFT))
-hs.hotkey.bind({"ctrl", "alt"}, "pad2", moveToGrid(BOT_FULL))
-hs.hotkey.bind({"ctrl", "alt"}, "pad3", moveToGrid(BOT_RIGHT))
-hs.hotkey.bind({"ctrl", "alt"}, "pad+", hs.grid.show)
-hs.hotkey.bind({"ctrl", "alt"}, "padenter",
+-- pass in a function to change the currently focused window.
+-- after the focus is changed, we'll draw a brief rect around the window
+-- to highlight it.
+function setFocus(fn)
+   return function()
+      fn()
+      local win = hs.window.focusedWindow()
+      if win then
+         local rect = hs.drawing.rectangle(win:frame())
+         rect:setFill(false)
+         rect:setStroke(true)
+         rect:setStrokeWidth(5)
+         rect:setStrokeColor(hs.drawing.color.osx_red)
+         rect:setLevel(hs.drawing.windowLevels["floating"])
+         rect:setRoundedRectRadii(4, 4)
+         rect:show()
+         rect:hide(0.65)
+         hs.timer.doAfter(0.65, function() rect:delete() end)
+      end
+   end
+end
+
+hs.hotkey.bind({"ctrl", "cmd"}, "left",  setFocus(hs.window.filter.focusWest))
+hs.hotkey.bind({"ctrl", "cmd"}, "right", setFocus(hs.window.filter.focusEast))
+hs.hotkey.bind({"ctrl", "cmd"}, "up",    setFocus(hs.window.filter.focusNorth))
+hs.hotkey.bind({"ctrl", "cmd"}, "down",  setFocus(hs.window.filter.focusSouth))
+
+-- window movement
+hs.hotkey.bind({"ctrl", "cmd"}, "pad7", moveToGrid(TOP_LEFT))
+hs.hotkey.bind({"ctrl", "cmd"}, "pad8", moveToGrid(TOP_FULL))
+hs.hotkey.bind({"ctrl", "cmd"}, "pad9", moveToGrid(TOP_RIGHT))
+hs.hotkey.bind({"ctrl", "cmd"}, "pad4", moveToGrid(FULL_LEFT))
+hs.hotkey.bind({"ctrl", "cmd"}, "pad5", moveToGrid(FULL_FULL))
+hs.hotkey.bind({"ctrl", "cmd"}, "pad6", moveToGrid(FULL_RIGHT))
+hs.hotkey.bind({"ctrl", "cmd"}, "pad1", moveToGrid(BOT_LEFT))
+hs.hotkey.bind({"ctrl", "cmd"}, "pad2", moveToGrid(BOT_FULL))
+hs.hotkey.bind({"ctrl", "cmd"}, "pad3", moveToGrid(BOT_RIGHT))
+hs.hotkey.bind({"ctrl", "cmd"}, "pad+", hs.grid.show)
+hs.hotkey.bind({"ctrl", "cmd"}, "padenter",
    function()
       local win = hs.window.frontmostWindow()
-      winToScreen(win, win:screen():next())
+      local screen = win:screen():next()
+      win:moveToScreen(screen)
    end
 )
 
