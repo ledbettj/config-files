c8c2675fa9d444ae5ff6c4ae24da9f7fcd8b84ff
John Ledbetter <john@callrail.com>
2018-10-11 16:24:03 -0400

    counsel/ivy/swiper

--- nextmacs.d/conf.d/init-minibuffer.el
+++ nextmacs.d/conf.d/init-minibuffer.el
@@ -2,3 +2,21 @@
 (setq history-length 1000)
 (setq minibuffer-prompt-properties
       (plist-put minibuffer-prompt-properties 'point-entered 'minibuffer-avoid-prompt))
+
+(use-package swiper :ensure t :defer t
+  :bind
+  (("C-s" . 'swiper)))
+
+(use-package ivy :ensure t :defer t
+  :bind
+  (:map ivy-minibuffer-map
+        ("RET" . 'ivy-alt-done)
+        ("C-j" . 'ivy-immediate-done))
+  :config
+  (setq ivy-extra-directories nil)
+  :init
+  (ivy-mode t))
+
+(use-package counsel :ensure t :defer t
+  :bind
+  (("C-c s" . counsel-git-grep)))
