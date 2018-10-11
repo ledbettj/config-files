de1317d0e5cb706c1fd7a70a4c88d794a49f2fea
John Ledbetter <john@callrail.com>
2018-10-11 11:57:31 -0400

    add company mode

---
+++ nextmacs.d/conf.d/init-company.el
@@ -0,0 +1,9 @@
+
+(use-package company :ensure t :defer t
+  :init (global-company-mode)
+  :config
+  (setq-default company-idle-delay                0.25)
+  (setq-default company-minimum-prefix-length     2)
+  (setq-default company-show-numbers              1)
+  (setq-default company-tooltip-align-annotations t))
+
