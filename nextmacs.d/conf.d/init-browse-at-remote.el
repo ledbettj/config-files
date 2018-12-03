50610e449aea59dcaa342f2a8fb1c82579de16c3
John Ledbetter <john@callrail.com>
2018-12-03 09:11:43 -0500

    yaml/browse-at-remote

---
+++ nextmacs.d/conf.d/init-browse-at-remote.el
@@ -0,0 +1,2 @@
+(use-package browse-at-remote :ensure t
+  :bind (("C-c h" . browse-at-remote)))
--- nextmacs.d/conf.d/init-ruby.el
+++ nextmacs.d/conf.d/init-ruby.el
@@ -1,3 +1,5 @@
 (use-package rspec-mode :ensure t)
 (use-package yard-mode :ensure t
   :hook ruby-mode)
+
+(use-package yaml-mode :ensure t)
