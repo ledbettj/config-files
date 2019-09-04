ab3943d05e7042c66671eac8a524aef225c5895c
John Ledbetter <john@callrail.com>
2019-09-04 09:15:05 -0400

    remove flycheck

--- nextmacs.d/conf.d/init-flycheck.el
+++
@@ -1,5 +0,0 @@
-(use-package flycheck :ensure t
-  :init (global-flycheck-mode)
-  :config
-  (setq flycheck-checkers (delete 'emacs-lisp-checkdoc flycheck-checkers))
-  (setq flycheck-indication-mode nil))
