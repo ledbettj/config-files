d3e17546e53b2bea381157f15e35e58acdbf1883
John Ledbetter <john.ledbetter@gmail.com>
2024-11-07 18:19:33 -0500

    doom.d

---
+++ doom.d/custom.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#2D2A2E" :foreground "#FCFCFA" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 135 :width normal :foundry "UKWN" :family "Iosevka")))))
