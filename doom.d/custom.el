16287aa36dc792fcac55aea5cc6765cc532dbb06
John Ledbetter <john.ledbetter@gmail.com>
2025-03-13 10:40:20 -0400

    disable format on save due to terrible codebases

--- doom.d/custom.el
+++ doom.d/custom.el
@@ -22,7 +22,8 @@
      "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e"
      "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec"
      "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710"
-     "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" default)))
+     "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" default))
+ '(safe-local-variable-values '((encoding . utf-8))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
